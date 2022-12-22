5f29febbad2df6459e290ceb13e9d0266748515e
Rliavots <RLiavots@its.jnj.com>
2022-12-22 20:42:15 +0300

    reverse int complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let reverseIntArray = [];
+    if (n > 0) {
+        for (let i = 0; i < String(n).length; i++){
+            reverseIntArray.unshift(String(n)[i]);
+    }
+        return(Number(reverseIntArray.join("")));
+    } else {
+        for (let i = 1; i < String(n).length; i++){
+            reverseIntArray.unshift(String(n)[i]);
+    }
+        return(Number(reverseIntArray.join("")));
+    }
 }
